088d6b3931914ce13475f52d3f63356841503532
Gib <goproductmgt@gmail.com>
2025-07-23 16:10:42 -0700

    Fix Authorization header typo in CORS

--- api/upload-mp3.js
+++ api/upload-mp3.js
@@ -747,7 +747,7 @@ export default async function handler(req, res) {
   // CORS configuration
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, uthorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
