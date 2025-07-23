cc05ec69a54763faeda478c8452e5dd2c36ff173
Gib <goproductmgt@gmail.com>
2025-07-23 14:10:36 -0700

    Fix CORS headers for frontend compatibility

--- api/upload-mp3.js
+++ api/upload-mp3.js
@@ -747,7 +747,7 @@ export default async function handler(req, res) {
   // CORS configuration
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, uthorization');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
